645690be9d233d74b3fbe5eb032e86154e463dcb
IWSR <lsh934960645@163.com>
2020-06-23 14:36:13 +0800

    feature: 波浪动画

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -6,14 +6,16 @@ import './style/index.less';
 class Index extends React.Component <IProp, IState> {
 
   private canvas: any = React.createRef();
+  private canvas2: any = React.createRef();
 
   componentDidMount() {
     const ctx = this.canvas.current.getContext('2d');
+    // const ctx2 = this.canvas2.current.getContext('2d');
     const dpr = 2;
-    const wave = new Wave(ctx, '#cccccc', 300 * dpr, 80 * dpr);
-    const wave2 = new Wave(ctx, '#a1a1a1', 300 * dpr, 80 * dpr, 30 * dpr);
-    wave2.animate();
+    const wave = new Wave(ctx, window.innerWidth, 80 * dpr);
+    // const wave2 = new Wave(ctx2, '#ffffff', 300 * dpr, 80 * dpr, Math.PI);
     wave.animate();
+    // wave.animate();
   }
 
   render() {
@@ -21,6 +23,7 @@ class Index extends React.Component <IProp, IState> {
       <div className='index-container'>
         <div className='above-fold'>
           <canvas ref={this.canvas} className="wave" />
+          <canvas ref={this.canvas2} className="wave" />
         </div>
         <div className='main-body'></div>
       </div>
--- src/pages/index/util/wave.ts
+++ src/pages/index/util/wave.ts
@@ -1,34 +1,47 @@
+/*
+  canvas进阶——如何画出平滑的曲线: https://juejin.im/post/5bc1a3f6f265da0ad947f4b1
+*/
 class Wave {
   private ctx: any
+  private currentTime: number
   private t: number
   private step: number
-  private color: string
-  private speed: number
   private a: number
-  private f: number
+  private stepsPreCicle: number
+  private stepsPreSecond: number
   private d: number
   private w: number
   private h: number
-  constructor(ctx: any, color: string, w: number, h: number, t: number = 0) {
+  private delay: number
+  constructor(ctx: any, w: number, h: number, delay: number = 0) {
     this.ctx = ctx;
-    this.t = t;
-    this.step = w / 300;
-    this.color = color;
-    this.speed = 0.1;
-    this.a = 30;
-    this.f = 20;
+    this.currentTime = Date.now();
+    this.t = 0;
+    this.step = w / 50;
+    // 振幅
+    this.a = 50;
+    // 垂直位移
     this.d = 20;
+    this.stepsPreSecond = 2;
+    // 每个周期多少step
+    this.stepsPreCicle = 5;
+    // canvas容器宽高
     this.w = w;
     this.h = h;
+    this.delay = delay;
   }
-  caculate(x: number) {
+  caculate(x: number, delay: number = 0) {
+    /*
+      正弦余弦函数 https://www.shuxuele.com/algebra/amplitude-period-frequency-phase-shift.html
+    */
+    // const amplitude = this.a;
+    const AngularV = (2 * Math.PI) / this.stepsPreCicle;
+    const stepsPreSecond = this.stepsPreSecond;
     const amplitude = this.a;
-    const displacement = this.d;
-    // A*sin(Bx + C) + D    
-    return amplitude * Math.sin(0.05 * x + this.t) + displacement;
+    return amplitude * Math.sin( AngularV * (x + stepsPreSecond * this.t + delay) );
   }
   render() {
-    this.t += this.speed;
+    this.t = (Date.now() - this.currentTime) / 1000;
     const ctx = this.ctx;
     const h = this.h;
     const w = this.w;
@@ -37,20 +50,67 @@ class Wave {
     ctx.translate(0, h / 2);
     ctx.lineWidth = 1;
     ctx.moveTo(0, 0);
+    let currentNode: any = null, nextNode: any = null;
     for (let i = this.step; i < w; i += this.step) {
-      ctx.lineTo(i, this.caculate(i));
+      if (i === this.step) {
+        currentNode = [i, this.caculate(i)];
+        nextNode = [i + this.step, this.caculate(i + this.step)];
+      } else {
+        currentNode = nextNode;
+        nextNode = [i + this.step, this.caculate(i + this.step)];
+      }
+      const computedNode = [(currentNode[0] + nextNode[0]) / 2, (currentNode[1] + nextNode[1]) / 2];
+      ctx.quadraticCurveTo(currentNode[0], currentNode[1], computedNode[0], computedNode[1]);
     }
     ctx.lineTo(w, this.caculate(w));
     ctx.lineTo(w, h);
     ctx.lineTo(0, h);
-    ctx.fillStyle = this.color;
+    ctx.fillStyle = 'rgba(255, 255, 255, 0.7)';
     ctx.fill();
     ctx.closePath();
     ctx.restore();
   }
 
+  render2() {
+    this.t = (Date.now() - this.currentTime) / 1000;
+    const ctx = this.ctx;
+    const h = this.h;
+    const w = this.w;
+    ctx.save();
+    ctx.beginPath();
+    ctx.translate(0, h / 2);
+    ctx.lineWidth = 1;
+    ctx.moveTo(0, 0);
+    let currentNode: any = null, nextNode: any = null;
+    for (let i = this.step; i < w; i += this.step) {
+      if (i === this.step) {
+        currentNode = [i, this.caculate(i, this.stepsPreCicle / 2)];
+        nextNode = [i + this.step, this.caculate(i + this.step, this.stepsPreCicle / 2)];
+      } else {
+        currentNode = nextNode;
+        nextNode = [i + this.step, this.caculate(i + this.step, this.stepsPreCicle / 2)];
+      }
+      const computedNode = [(currentNode[0] + nextNode[0]) / 2, (currentNode[1] + nextNode[1]) / 2];
+      ctx.quadraticCurveTo(currentNode[0], currentNode[1], computedNode[0], computedNode[1]);
+    }
+    ctx.lineTo(w, this.caculate(w, this.stepsPreCicle / 2));
+    ctx.lineTo(w, h);
+    ctx.lineTo(0, h);
+    ctx.fillStyle = 'rgba(193, 193, 193, 0.4)';
+    ctx.fill();
+    ctx.closePath();
+    ctx.restore();
+  }
+
+  renderBoth() {
+    this.ctx.clearRect(0, 0, this.w, this.h);
+    this.render();
+    this.render2();
+    requestAnimationFrame(this.renderBoth.bind(this));
+  }
+
   animate() {
-    requestAnimationFrame(this.render.bind(this));
+    this.renderBoth();
   }
 }
 
